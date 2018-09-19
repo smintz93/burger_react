09f68aa798c99929ea9a6a6ae1dc415574e503f9
Sam <sammintz1993@gmail.com>
2018-09-19 13:18:42 -0500

    burger displayed on page with proper css

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from '.BurgerIngredient.css'
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
 	render(){
@@ -13,22 +13,22 @@ class BurgerIngredient extends Component {
 			case ('bread-top'):
 				ingredient = (
 					<div className={classes.BreadTop}>
-						<div className={classes.Seeds1}</div>
-						<div className={classes.Seeds2}</div>
+						<div className={classes.Seeds1}></div>
+						<div className={classes.Seeds2}></div>
 					</div>
-				);	
+				)
 				break;
 			case ('meat'):
-				ingredient = <div className={classes.Meat}</div>
+				ingredient = <div className={classes.Meat}></div>
 				break;
 			case ('cheese'):
-				ingredient = <div className={classes.Cheese}</div>
+				ingredient = <div className={classes.Cheese}></div>
 				break;		
 			case ('salad'):
-				ingredient = <div className={classes.Salad}</div>
+				ingredient = <div className={classes.Salad}></div>
 				break;
 			case ('bacon'):
-				ingredient = <div className={classes.Bacon}</div>
+				ingredient = <div className={classes.Bacon}></div>
 				break;	
 			default:
 				ingredient = null;					
