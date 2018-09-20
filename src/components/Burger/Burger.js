1034e275565cae3f1e913c02ac299732dc7c60c0
Sam <sammintz1993@gmail.com>
2018-09-20 09:17:12 -0500

    converted ing from str to arr to map over

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	console.log(props, "this is props")
+	// ingreidents from state is not an array
+	// need to transform it to an array
+	console.log(props.ingredients, "this is props.ingreidents")
+	const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+			return <BurgerIngredient key={igKey + i} type={igKey}/>
+		});
+	});
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);	
