4c0fee210e4a089779025c2bd75b8a8875460d80
Sam <sammintz1993@gmail.com>
2018-09-20 09:29:11 -0500

    reduced array and added conditional render for 0 elements

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	console.log(props, "this is props")
 	// ingreidents from state is not an array
 	// need to transform it to an array
-	console.log(props.ingredients, "this is props.ingreidents")
-	const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+	let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
 		return [...Array(props.ingredients[igKey])].map((_, i) => {
 			return <BurgerIngredient key={igKey + i} type={igKey}/>
 		});
-	});
+	})
+	//flattend the array so its just one array instead of four arrays
+	.reduce((arr, el) => {
+		//take given element and add it to array 
+		return arr.concat(el)
+	}, [])
+	if(transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients</p>
+	}
+
+	console.log(transformedIngredients);
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,18 +11,17 @@ class BurgerBuilder extends Component  {
 	// }
 	state = {
 		ingredients: {
-			salad: 1,
-			bacon: 1,
-			cheese: 2,
-			meat: 2
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
 		}
-
 	}
 	render(){
-		console.log(this.state.ingredients)
+		console.log(this.state.ingredients, 'this is state in Burger Builder')
 		return(
 			<Aux>
-				<Burger ingredients={this.state.ingredients} />
+				<Burger ingredients={this.state.ingredients}/>
 				<div>Build Controls</div>
 			</Aux>
 		);
