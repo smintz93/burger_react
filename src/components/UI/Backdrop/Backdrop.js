adc2b8c21ff4d0f197728c6d0d25dec124865d17
Sam <sammintz1993@gmail.com>
2018-09-27 09:27:11 -0500

    created backdrop for when modal is open

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,1 +1,10 @@
-Backdrop.js
+import React from 'react';
+
+import classes from './Backdrop.css'
+
+const backdrop = (props) => (
+	props.show ? <div className={classes.Backdrop}></div> : null
+
+);
+
+export default backdrop;
