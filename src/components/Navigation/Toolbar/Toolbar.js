ee0248c6a2605ef0fd82a777accfdf75ba80f9ce
Sam <sammintz1993@gmail.com>
2018-10-02 12:55:26 -0500

    toolbar displayed in layout comp

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Toolbar.css'
 
 const toolbar = () => (
-	<header>
+	<header className={classes.Toolbar}>
 		<div>MENU</div>
 		<div>LOGO</div>
 		<nav>
