5641241e67761134d9ace077082cbd1b894b2ee6
Sam <sammintz1993@gmail.com>
2018-11-07 11:03:35 -0600

    spinner added for when continue is selected inside the modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,7 @@ import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component {
 	shouldComponentUpdate(nextProps, nextState){
 		// only update modal if changes have been made 
-		return  nextProps.show !== this.props.show;
+		return  nextProps.show !== this.props.show || nextProps.children !== this.props.children;
 	}
 
 	componentWillUpdate(){
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -30,6 +31,7 @@ class BurgerBuilder extends Component  {
 		totalPrice: 4,
 		purchaseable: false,
 		purchasing: false, 
+		loading: false
 	}
 
 	updatePurchaseState = (ingredients) => {
@@ -87,6 +89,7 @@ class BurgerBuilder extends Component  {
 	}
 	purchaseContinue = () => {
 		// alert('You continue');
+		this.setState({loading: true})
 		const order = {
 			ingredients: this.state.ingredients,
 			price: this.state.totalPrice,
@@ -102,9 +105,12 @@ class BurgerBuilder extends Component  {
 			deliveryMethod: 'fastest'
 		}
 		axios.post('/orders.json', order) // for firebase to work correctly need .json 
-			.then(response => console.log(response))
-			.catch(error => console.log(response));
-
+			.then(response => {
+				this.setState({loading: false, purchasing: false});
+			})
+			.catch(error => {
+				this.setState({loading: false, purchasing: false});
+			});
 
 	}
 	render(){
@@ -117,14 +123,19 @@ class BurgerBuilder extends Component  {
 		}
 		// {salad: true, meat: false, ... }
 		// console.log(this.state.ingredients, 'this is state in Burger Builder')
-		return(
-			<Aux>
-				<Modal show={this.state.purchasing} modalClosed={this.closeModal}>
-					<OrderSummary 
+		let orderSummary = 	<OrderSummary 
 						ingredients={this.state.ingredients} 
 						closePurchase={this.closeModal}
 						continuePurchase={this.purchaseContinue}
 						price={this.state.totalPrice}/>
+
+		if(this.state.loading){
+			orderSummary = <Spinner />
+		}
+		return(
+			<Aux>
+				<Modal show={this.state.purchasing} modalClosed={this.closeModal}>
+					{orderSummary}
 				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls 
