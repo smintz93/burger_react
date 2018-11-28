aa39a3e5af76ac1a8cfdad4d910dfd7d5f81a456
Sam <sammintz1993@gmail.com>
2018-11-28 10:24:42 -0600

    added checkout form

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,20 @@
+// stateful 
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+
+	state = {
+
+	}
+
+	render(){
+
+		return (
+			<div>
+			</div>
+		);
+	}
+}
+
+export default Checkout;
+
