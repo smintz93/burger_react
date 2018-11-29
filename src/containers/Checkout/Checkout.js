d7b300df1ed02ae8240541cc153e4dce076c91df
Sam <sammintz1993@gmail.com>
2018-11-29 09:45:42 -0600

    added form to contact data page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,9 @@
 // stateful 
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+
 
 
 class Checkout extends Component {
@@ -42,6 +44,8 @@ class Checkout extends Component {
 					cancelCheckout={this.checkoutCancelled} 
 					continueCheckout={this.chceckoutContinued} 
 					ingredients={this.state.ingredients}/>
+				<Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+
 			</div>
 		);
 	}
