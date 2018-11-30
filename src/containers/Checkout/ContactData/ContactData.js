ddbca83f5fa870e8bf0d8e15b40d2a583910517d
Sam <sammintz1993@gmail.com>
2018-11-30 09:35:11 -0600

    Changed compDidMount to compWillMount in checkout when ingreds are initially null

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css'
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 	state = {
@@ -10,12 +11,34 @@ class ContactData extends Component {
 		address: {
 			street: '',
 			postalCode: ''
-		}
+		},
+		loading: false
 	}
 
 	orderHandler = (e) => {
 		e.preventDefault();
-		console.log(this.props.ingredients, "props in orderHandler")
+		this.setState({loading: true})
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.price,
+			customer: {
+				name: 'Sam Mintz',
+				address: {
+					street: 'Teststreet 1',
+					zipcode: '60610',
+					country: 'USA'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		}	
+		axios.post('/orders.json', order) // for firebase to work correctly need .json 
+			.then(response => {
+				this.setState({loading: false, purchasing: false});
+			})
+			.catch(error => {
+				this.setState({loading: false, purchasing: false});
+			});
 	}
 
 	render() {
