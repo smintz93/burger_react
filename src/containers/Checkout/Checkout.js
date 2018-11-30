0a8da1bbb7c262b59d232ffab82ff8b28fcbd07b
Sam <sammintz1993@gmail.com>
2018-11-30 09:42:40 -0600

    after checkout, user gets redirected back to home page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -47,10 +47,12 @@ class Checkout extends Component {
 					ingredients={this.state.ingredients}/>
 				<Route 
 				path={this.props.match.path + '/contact-data'} 
-				render={() => 
+				render={(props) => 
 					(<ContactData 
 						ingredients={this.state.ingredients} 
-						price={this.state.totalPrice}/>)}/>
+						price={this.state.totalPrice}
+						{...props}/>)}
+						/>
 
 			</div>
 		);
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
 
@@ -34,24 +35,30 @@ class ContactData extends Component {
 		}	
 		axios.post('/orders.json', order) // for firebase to work correctly need .json 
 			.then(response => {
-				this.setState({loading: false, purchasing: false});
+				this.setState({loading: false});
+				this.props.history.push('/');
 			})
 			.catch(error => {
-				this.setState({loading: false, purchasing: false});
+				this.setState({loading: false});
 			});
 	}
 
 	render() {
+		let form = (
+			<form>
+				<input className={classes.Input }type="text" name="name" placeholder="Your name" />
+				<input className={classes.Input }type="email" name="email" placeholder="Your email" />
+				<input className={classes.Input }type="text" name="street" placeholder="Your street" />
+				<input className={classes.Input }type="text" name="postal" placeholder="Your postal code" />
+				<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+			</form>);
+		if(this.state.loading){
+			form = <Spinner />;
+		}
 		return (
 			<div className={classes.ContactData}>
 				<h4>Enter Your Contact Info</h4>
-				<form>
-					<input className={classes.Input }type="text" name="name" placeholder="Your name" />
-					<input className={classes.Input }type="email" name="email" placeholder="Your email" />
-					<input className={classes.Input }type="text" name="street" placeholder="Your street" />
-					<input className={classes.Input }type="text" name="postal" placeholder="Your postal code" />
-					<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
-				</form>
+					{form}
 			</div>
 		);
 	}
