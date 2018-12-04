ebdc35a97e6f9d0185398da3ed5cfc7121039eb4
Sam <sammintz1993@gmail.com>
2018-12-04 10:44:06 -0600

    Input comp used in contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
@@ -46,12 +47,13 @@ class ContactData extends Component {
 	render() {
 		let form = (
 			<form>
-				<input className={classes.Input }type="text" name="name" placeholder="Your name" />
-				<input className={classes.Input }type="email" name="email" placeholder="Your email" />
-				<input className={classes.Input }type="text" name="street" placeholder="Your street" />
-				<input className={classes.Input }type="text" name="postal" placeholder="Your postal code" />
+				<Input inputype="input" name="name" placeholder="Your name" />
+				<Input inputype="input" name="email" placeholder="Your email" />
+				<Input inputype="input" name="street" placeholder="Your street" />
+				<Input inputype="input" name="postal" placeholder="Your postal code" />
 				<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
-			</form>);
+			</form>
+		);
 		if(this.state.loading){
 			form = <Spinner />;
 		}
