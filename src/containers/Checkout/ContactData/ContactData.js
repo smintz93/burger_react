536163e765b16dca46759083f30bc93f22da9b1c
Sam <sammintz1993@gmail.com>
2018-12-04 12:36:08 -0600

    can send form data to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-import classes from './ContactData.css'
+import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
-	 state = {
+    state = {
         orderForm: {
             name: {
                 elementType: 'input',
@@ -63,57 +63,71 @@ class ContactData extends Component {
         loading: false
     }
 
-	orderHandler = (e) => {
-		e.preventDefault();
-		this.setState({loading: true})
-		const order = {
-			ingredients: this.props.ingredients,
-			price: this.props.price,
-		}	
-		axios.post('/orders.json', order) // for firebase to work correctly need .json 
-			.then(response => {
-				this.setState({loading: false});
-				this.props.history.push('/');
-			})
-			.catch(error => {
-				this.setState({loading: false});
-			});
-	}
+    orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push( '/' );
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
+    }
 
-	render() {
-		// dynamically rendering the input fields
-		const formElementArray = [];
-		// now have an array of js objects
-		for (let key in this.state.orderForm) {
-			formElementArray.push({
-				id: key,
-				config: this.state.orderForm[key]
-			});
-		}
-		let form = (
-			<form>
-				{formElementArray.map(formElement => (
-					<Input 
-						key={formElement.id}
-						elementType={formElement.config.elementType}
-						elementConfig={formElement.config.elementConfig}
-						value={formElement.config.value}/> 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = { 
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
 
-				))}
-				<Button btnType="Success" clicked={this.orderHandler}>Order</Button>
-			</form>
-		);
-		if(this.state.loading){
-			form = <Spinner />;
-		}
-		return (
-			<div className={classes.ContactData}>
-				<h4>Enter Your Contact Info</h4>
-					{form}
-			</div>
-		);
-	}
+    render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form = (
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success">ORDER</Button>
+            </form>
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
 }
 
-
-export default ContactData; 
+export default ContactData;
