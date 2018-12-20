1e57eb0d3fad0f331e6dcf752fd60269285ed13f
Sam <sammintz1993@gmail.com>
2018-12-20 13:03:26 -0600

    created action creator for ordering a burger

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,2 +1,32 @@
 // action creators for submitting an order
 
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+	return {
+		type: actionTypes.PURCHASE_BURGER_SUCCESS,
+		orderId: id,
+		orderData: orderData
+	}
+};
+
+export const purchaseBurgerFail = (error) => {
+	return {
+		type: actionTypes.PURCHASE_BURGER_FAIL,
+		error: error
+	}
+};
+
+export const purchaseBurgerStart = (orderData) => {
+	return (dispatch) => {
+		  axios.post( '/orders.json', orderData )
+            .then(response => {
+            	console.log(response.data, 'this is response.data')
+               dispatch(purchaseBurgerSuccess(response.data, orderData))
+            } )
+            .catch(error => {
+               dispatch(purchaseBurgerFail(error))
+            } );
+	}
+};
